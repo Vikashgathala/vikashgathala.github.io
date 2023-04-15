66eb0c63e959a47f1771e8d7b93a16fb0fbc90dd
vikashgathala <vikashgathala@gmail.com>
2023-04-15 18:41:17 +0530

    add blur effect to js activity

--- style.js
+++ style.js
@@ -1,14 +1,34 @@
+// window.onload = function() {
+// var text = document.getElementById("dynamic-text");
+// var words = ["Student", "Vikash", "Coder", "Learner"];
+// var index = 0;
+// var span = text.querySelector("span");
+// setInterval(function() {
+// span.classList.remove("show");
+// setTimeout(function() {
+// span.innerHTML = words[index];
+// index = (index + 1) % words.length;
+// span.classList.add("show");
+// }, 380);
+// }, 2000);
+// };
 window.onload = function() {
-var text = document.getElementById("dynamic-text");
-var words = ["Student", "Vikash", "Coder", "Learner"];
-var index = 0;
-var span = text.querySelector("span");
-setInterval(function() {
-span.classList.remove("show");
-setTimeout(function() {
-span.innerHTML = words[index];
-index = (index + 1) % words.length;
-span.classList.add("show");
-}, 380);
-}, 2000);
+  var text = document.getElementById("dynamic-text");
+  var words = ["Student", "Vikash", "Coder", "Learner"];
+  var index = 0;
+  var span = text.querySelector("span");
+  setInterval(function() {
+    span.classList.remove("show");
+    setTimeout(function() {
+      span.innerHTML = words[index];
+      index = (index + 1) % words.length;
+      span.classList.add("show");
+      span.classList.add("blur");
+      setTimeout(function() {
+        span.classList.remove("blur");
+        span.style.animation = '';
+      }, 1000);
+      span.style.animation = 'unblur 0.8s ease-out forwards';
+    }, 400);
+  }, 2500);
 };
