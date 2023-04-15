84734be17d8cb47019f4153b42714efcfe08d615
vikashgathala <vikashgathala@gmail.com>
2023-04-15 18:42:09 +0530

    no msg

--- style.js
+++ style.js
@@ -1,17 +1,3 @@
-// window.onload = function() {
-// var text = document.getElementById("dynamic-text");
-// var words = ["Student", "Vikash", "Coder", "Learner"];
-// var index = 0;
-// var span = text.querySelector("span");
-// setInterval(function() {
-// span.classList.remove("show");
-// setTimeout(function() {
-// span.innerHTML = words[index];
-// index = (index + 1) % words.length;
-// span.classList.add("show");
-// }, 380);
-// }, 2000);
-// };
 window.onload = function() {
   var text = document.getElementById("dynamic-text");
   var words = ["Student", "Vikash", "Coder", "Learner"];
