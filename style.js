db725c5424bc660dac6999f0a0ad285c3b914576
vikashgathala <vikashgathala@gmail.com>
2023-04-15 14:50:55 +0530

    removed more spaces

--- style.js
+++ style.js
@@ -1,17 +1,14 @@
-	// alert("This Portfolio is optimized for Desktop mainly. Make sure to clear your browser caches timely to fetch the website with updated resources. High end device is recommended for smooth animations.");
-
-	window.onload = function() {
-			var text = document.getElementById("dynamic-text");
-			var words = ["Student", "Vikash", "Coder", "Learner"];
-			var index = 0;
-			var span = text.querySelector("span");
-
-			setInterval(function() {
-				span.classList.remove("show");
-				setTimeout(function() {
-					span.innerHTML = words[index];
-					index = (index + 1) % words.length;
-					span.classList.add("show");
-				}, 380);
-			}, 2000);
-		};
+window.onload = function() {
+var text = document.getElementById("dynamic-text");
+var words = ["Student", "Vikash", "Coder", "Learner"];
+var index = 0;
+var span = text.querySelector("span");
+setInterval(function() {
+span.classList.remove("show");
+setTimeout(function() {
+span.innerHTML = words[index];
+index = (index + 1) % words.length;
+span.classList.add("show");
+}, 380);
+}, 2000);
+};
